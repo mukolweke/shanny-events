5e70f57818a95e785ecd36119db42f3e422cd44a
mukolweke <michaelolukak@gmail.com>
2019-02-16 18:46:21 +0300

    landing page done

--- client/client_page.php
+++ client/client_page.php
@@ -3,9 +3,11 @@ session_start();
 
 // Check if the user is logged in;
 if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
-    header("location: login.php");
+    header("location: /auth/login.php");
     exit;
 }
+
+$active_page = 'events'
 
 ?>
 
@@ -19,26 +21,45 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
 </head>
 <body>
 <div class="topnav">
-    <a href="../index.html">Home</a>
+    <div class="container">
+        <a href="../index.html">Home</a>
 
-    <?php if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true)
-        echo '<a href="../auth/login.php">Login</a>'
-    ?>
+        <?php if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true)
+            echo '<a href="../auth/login.php">Login</a>'
+        ?>
 
-    <?php if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true)
-        echo '<a href="../auth/register.php">Register</a>'
-    ?>
+        <?php if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true)
+            echo '<a href="../auth/register.php">Register</a>'
+        ?>
 
-    <?php if (isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] === true)
-        echo '<a href="../auth/logout.php">Logout</a>'
-    ?>
-
+        <?php if (isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] === true)
+            echo '<a href="../auth/logout.php">Logout</a>'
+        ?>
+    </div>
 </div>
 
 <div class="container">
-    <div class="wrapper">
-        <h1>Welcome Client</h1>
+    <div class="dash-body">
+        <div class="side-menu">
+            <div id="mySidenav" class="sidenav">
+                <a href="#">Events</a>
+                <a href="#">Profile</a>
+            </div>
+        </div>
+
+        <div class="main-content">
+            <?php
+            if ($active_page == 'events') {
+                include 'events.php';
+            } else {
+                include 'profile.php';
+            }
+            ?>
+
+        </div>
     </div>
 </div>
 </body>
 </html>
+
+
